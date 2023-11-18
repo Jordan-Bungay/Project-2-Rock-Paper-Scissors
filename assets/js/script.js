28fe04aa5a5881a60d63d1be36c4c2791f4ebaa8
Jordan-Bungay <>
2023-11-18 21:44:38 +0000

    Add a function to decide a winner of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,33 @@ let win = 0;
 let lose = 0;
 let draw = 0;
 
+// Deciding a winner
+function setWinner(player, computer) {
+    let result = '';
+    if (player === computer) {
+        result = 'Draw!';
+    } else {
+        switch (player) {
+            case 'rock':
+                result = computer === 'scissors' ? 'You Win!' : 'You Lose...';
+                break;
+            case 'paper':
+                result = computer === 'rock' ? 'You Win!' : 'You Lose...';
+                break;
+            case 'scissors':
+                result = computer === 'paper' ? 'You Win!' : 'You Lose...';
+                break;
+        }
+        if (result === 'You Win!') {
+            win++;
+            updateWin();
+        } else if (result === 'You Lose...') {
+            lose++
+            updateLose();
+        } else if (result === 'Draw!') {
+            draw++;
+            updateDraw();
+        }
+    }
+    return result;
+}
